e30217900c5a11915f165247739b2e1ee6e90ebd
Timothy Crosley <timothy.crosley@gmail.com>
2016-04-25 21:28:45 -0700

    Add connection logic

---
+++ hug_peewee/connect.py
@@ -0,0 +1,15 @@
+import peewee
+from peewee import *
+
+def connect(database='SqlliteDatabse', location=':memory:', **settings):
+    try:
+        database_class = load_class(database_config)
+        assert issubclass(database_class, database)
+    except ImportError:
+        raise RuntimeError('Unable to import: "{}"'.format(database))
+    except AttributeError:
+        raise RuntimeError('Database engine not found: "{}"'.format(database))
+    except AssertionError:
+        raise ValueError('Database engine not a subclass of peewee.Database: "{}"'.format(database))
+
+    return database_class(location, **settings)

a59db72a99dc39c4dcf512b408c28c45642e339d
Timothy Crosley <timothy.crosley@gmail.com>
2016-04-27 23:44:09 -0700

    Add basic connection testing

--- tests/test_connnection.py
+++ tests/test_connnection.py
@@ -16,6 +16,6 @@ def fake_endpoint():
 def test_manage():
     """Test to ensure telling hug_peewee to manage connections works as expected"""
     assert isinstance(hug_peewee.connection.manage(api), SqliteDatabase)
-    assert isinstance(hug_peewee.connection.manage(api, location='here'), SqliteDatabase)
+    assert isinstance(hug_peewee.connection.manage(api, location='connection_testing.db'), SqliteDatabase)
     assert hug.test.get(api, 'fake_endpoint').data == True
 
